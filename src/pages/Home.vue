58d66a3de3b598b67a68add7a13553df05cc5383
Aku Kettunen <aku@kettunen.com>
2025-09-05 08:15:23 +0300

    feat: add home page title

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,46 @@
 <template>
   <v-container>
-    <div class="text-h1">{{ $t('home.welcome') }}</div>
+    <div class="text-h3 mt-4">{{ greeting }}! {{ currentEmoji }}</div>
   </v-container>
 </template>
+
+<script lang="ts">
+  import { useAuth0 } from '@auth0/auth0-vue'
+
+  export default {
+    name: 'Home',
+    setup() {
+      const { user } = useAuth0()
+      return { user }
+    },
+    data() {
+      return {
+        positiveEmojis: [
+          '💪', '🔥', '⚡', '🚀', '🎯', '🏆', '💎', '⭐',
+          '🌲', '🦅', '🐺', '🦁', '🐻', '🦊', '🎸'
+        ]
+      }
+    },
+    computed: {
+      greeting() {
+        const hour = new Date().getHours()
+
+        let timeKey = 'goodMorning'
+        if (hour >= 12 && hour < 17) {
+          timeKey = 'goodAfternoon'
+        } else if (hour >= 17 && hour < 22) {
+          timeKey = 'goodEvening'
+        } else if (hour >= 22 || hour < 6) {
+          timeKey = 'goodNight'
+        }
+
+        return this.$t(`home.${timeKey}`)
+      },
+      currentEmoji() {
+        const hour = new Date().getHours()
+        const emojiIndex = hour % this.positiveEmojis.length
+        return this.positiveEmojis[emojiIndex]
+      }
+    }
+  }
+</script>
